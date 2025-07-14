945b9d0c331c6969ddb56cbb29bf6077a1ff094a
Gökalp <m.emin0012@gmail.com>
2025-07-14 16:37:40 +0300

    Initial commit

---
+++ src/main/java/hafta1/VarMain.java
@@ -0,0 +1,29 @@
+package hafta1;
+
+public class VarMain {
+
+    public static void main(String[] args) {
+
+    int var = 123; //
+
+    //String void = "Patika"; void ile değişken tanımı yapılamaz.
+
+    var name = "gokalp";
+
+    var age = 23;
+
+    // var value : Geçersiz kullanım Çünkü İlk değer ataması yok
+
+    var value = 10;
+
+    // value = "Patika"; compile time hatası çünkü bir üst satırda int olduğunu belirttik
+
+    name = "Patika";
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/hafta1/main.java
@@ -0,0 +1,89 @@
+package hafta1;
+
+public class main {
+
+
+public static void main(String[] args) {
+
+    System.out.println("Hello Patika Dev1");
+
+    System.out.println("Hello Patika Dev2");
+
+    System.out.println("Hello Patika Dev3");
+
+    int i = 30;
+
+    int i1 = 50;
+
+    boolean isA;
+
+    isA = true;
+
+    isA = false;
+
+  i = -5000;
+
+  i1 = 300 + i;
+
+  byte b =127 ;
+
+  b = -128 ;
+
+  char c = 'c';
+
+    System.out.println(c);
+
+    char c1 = 'A';
+
+    System.out.println(c1);
+
+    int i3 = 2147483647;
+
+    long l;
+
+    l = 200;
+
+    long l1 = 100;
+
+    int sum = 100 + 200;
+
+    int x = 10 ;
+    int y = 20 ;
+
+    int sum1 = x+ y;
+
+    System.out.println("Toplam:" + sum1);
+
+    int result= x - y ;
+
+    System.out.println("Çıkarma:" + result);
+
+
+    int result1 = x + y;
+    System.out.println("Çarpma" + result1);
+
+    int result2 = x / y;
+    System.out.println("Bölme" + result2);
+
+    int result3 = x % y;
+    System.out.println("mod" + result3);
+
+    boolean isEquals = x == y;
+    boolean isEquals1 = 10 == y;
+
+    boolean isEquals2 = x != y;
+    boolean Greather = x > 20;
+    boolean isEqualsOrGreather = x >= 20;
+    boolean isLessThan = x < 5;
+    boolean isEqualsOrLess = x <= result2;
+
+    boolean isTrue = isEquals && isEquals1;
+    boolean isTrue1 = true && false;
+
+    boolean isFalse = false || true;
+    boolean isFalse1 = (false || true) && (false && false) || (true && true && false);
+
+
+
+}
+}
